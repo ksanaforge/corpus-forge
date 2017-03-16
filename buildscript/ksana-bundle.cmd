69904adb96b8ff7e5cb212c4572998ec8cd8d6b5
yapcheahshen <yapcheahshen@gmail.com>
2017-03-16 18:41:33 +0800

    exclude ksana-corpus-lib

--- buildscript/ksana-bundle.cmd
+++ buildscript/ksana-bundle.cmd
@@ -1,2 +1,2 @@
-browserify -g [envify --NODE_ENV 'production'] -r ksana-corpus-rom -r ksana-corpus -r ksana-corpus-search -r ksana-corpus-builder -x react-native | uglifyjs --screw-ie8 -c=dead_code,evaluate,loops,unused -m > ../static/ksana-bundle.min.js
-rem browserify -g [envify --NODE_ENV 'production'] -r ksana-corpus-rom -r ksana-corpus -r ksana-corpus-search -r ksana-corpus-builder -x react-native  > ../static/ksana-bundle.js
+browserify -g [envify --NODE_ENV 'production'] -r ksana-corpus-rom -r ksana-corpus -r ksana-corpus-search -r ksana-corpus-builder -r ksana-unihan-variant -x react-native | uglifyjs --screw-ie8 -c=dead_code,evaluate,loops,unused -m > ../static/ksana-bundle.min.js
+rem browserify -g [envify --NODE_ENV 'production'] -r ksana-corpus-rom -r ksana-corpus -r ksana-corpus-search -r ksana-corpus-builder -x react-native  > ../static/ksana-bundle. 
